80bbacfbc33c3ce1aa9f54d866d4de020903deb0
Anastasia Les <anastasia@sonarmedia.com>
2022-04-17 12:29:01 +0200

    fixed searchbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { NameField } from '../Form/Form';
 import { Searchbar } from '../Searchbar/Searchbar';
-import { FilterField } from '../Filter/filter';
 
-const LS_KEY = 'added_contacts';
 const URL = 'https://pixabay.com/api/?key=25256496-da285e9dc7351a7d44328e376&q=yellow+flowers&image_type=photo';
 
 export class ImageFinder extends React.Component {
